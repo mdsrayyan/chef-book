91bd29459fdc93a2a4ec9da0650419e897bd19f9
Mohammad Sirajuddin Rayyan <mds0060@gmail.com>
2021-03-23 20:04:11 +0100

    dialog component pushed

---
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -0,0 +1,30 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ConfirmationDialogComponent} from './confirmation-dialog.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+
+describe('ConfirmationDialogComponent', () => {
+  let component: ConfirmationDialogComponent;
+  let fixture: ComponentFixture<ConfirmationDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+        ],
+      declarations: [ConfirmationDialogComponent]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfirmationDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
